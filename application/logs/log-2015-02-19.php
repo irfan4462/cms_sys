3f84d8a5068880c95b5592ef7a3046bab4189d8e
irfan44624 <irfan474728@gmail.com>
2015-02-20 11:32:21 +0500

    application folder push

---
+++ application/logs/log-2015-02-19.php
@@ -0,0 +1,24 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2015-02-19 12:00:43 --> Severity: Warning  --> mysql_connect(): Access denied for user 'CHANGEME'@'localhost' (using password: YES) C:\xampp\htdocs\cms_ci\system\database\drivers\mysql\mysql_driver.php 73
+ERROR - 2015-02-19 12:00:44 --> Unable to connect to the database
+ERROR - 2015-02-19 12:00:45 --> Severity: Warning  --> mysql_connect(): Access denied for user 'CHANGEME'@'localhost' (using password: YES) C:\xampp\htdocs\cms_ci\system\database\drivers\mysql\mysql_driver.php 73
+ERROR - 2015-02-19 12:00:45 --> Unable to connect to the database
+ERROR - 2015-02-19 12:00:45 --> Severity: Warning  --> mysql_query() expects parameter 2 to be resource, boolean given C:\xampp\htdocs\cms_ci\system\database\drivers\mysql\mysql_driver.php 179
+ERROR - 2015-02-19 12:00:45 --> Severity: Warning  --> mysql_num_rows() expects parameter 1 to be resource, null given C:\xampp\htdocs\cms_ci\system\database\drivers\mysql\mysql_result.php 37
+ERROR - 2015-02-19 12:00:45 --> Severity: Warning  --> mysql_num_rows() expects parameter 1 to be resource, null given C:\xampp\htdocs\cms_ci\system\database\drivers\mysql\mysql_result.php 37
+ERROR - 2015-02-19 12:00:45 --> Severity: 8192  --> mysql_escape_string(): This function is deprecated; use mysql_real_escape_string() instead. C:\xampp\htdocs\cms_ci\system\database\drivers\mysql\mysql_driver.php 319
+ERROR - 2015-02-19 12:00:46 --> Severity: Warning  --> mysql_connect(): Access denied for user 'CHANGEME'@'localhost' (using password: YES) C:\xampp\htdocs\cms_ci\system\database\drivers\mysql\mysql_driver.php 73
+ERROR - 2015-02-19 12:00:46 --> Unable to connect to the database
+ERROR - 2015-02-19 12:00:46 --> Severity: Warning  --> mysql_query() expects parameter 2 to be resource, boolean given C:\xampp\htdocs\cms_ci\system\database\drivers\mysql\mysql_driver.php 179
+ERROR - 2015-02-19 12:00:46 --> Severity: Warning  --> mysql_num_rows() expects parameter 1 to be resource, null given C:\xampp\htdocs\cms_ci\system\database\drivers\mysql\mysql_result.php 37
+ERROR - 2015-02-19 12:00:46 --> Severity: Warning  --> mysql_num_rows() expects parameter 1 to be resource, null given C:\xampp\htdocs\cms_ci\system\database\drivers\mysql\mysql_result.php 37
+ERROR - 2015-02-19 12:00:46 --> Severity: 8192  --> mysql_escape_string(): This function is deprecated; use mysql_real_escape_string() instead. C:\xampp\htdocs\cms_ci\system\database\drivers\mysql\mysql_driver.php 319
+ERROR - 2015-02-19 12:00:47 --> Severity: Warning  --> mysql_connect(): Access denied for user 'CHANGEME'@'localhost' (using password: YES) C:\xampp\htdocs\cms_ci\system\database\drivers\mysql\mysql_driver.php 73
+ERROR - 2015-02-19 12:00:47 --> Unable to connect to the database
+ERROR - 2015-02-19 12:00:47 --> Severity: Warning  --> mysql_query() expects parameter 2 to be resource, boolean given C:\xampp\htdocs\cms_ci\system\database\drivers\mysql\mysql_driver.php 179
+ERROR - 2015-02-19 12:00:47 --> Severity: Warning  --> mysql_num_rows() expects parameter 1 to be resource, null given C:\xampp\htdocs\cms_ci\system\database\drivers\mysql\mysql_result.php 37
+ERROR - 2015-02-19 12:00:47 --> Severity: Warning  --> mysql_num_rows() expects parameter 1 to be resource, null given C:\xampp\htdocs\cms_ci\system\database\drivers\mysql\mysql_result.php 37
+ERROR - 2015-02-19 12:00:47 --> 404 Page Not Found --> http://localhost/cms_ci/public_html/
+ERROR - 2015-02-19 12:22:11 --> Severity: Notice  --> unserialize(): Error at offset 0 of 320 bytes C:\xampp\htdocs\ci_web\system\libraries\Session.php 724
+ERROR - 2015-02-19 12:22:11 --> Query error: Table 'cms_ci.ci_sessions' doesn't exist
